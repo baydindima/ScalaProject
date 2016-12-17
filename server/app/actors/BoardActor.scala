4f1e42f098c1df952aca8fad643baaa19353c964
ilnurshug <ilnur.shugaepov@mail.ru>
2016-12-18 01:58:35 +0300

    Add actors

---
+++ server/app/actors/BoardActor.scala
@@ -0,0 +1,29 @@
+package actors
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
+import akka.event.LoggingReceive
+import play.libs.Akka
+
+class BoardActor extends Actor with ActorLogging {
+  var users = Set[ActorRef]()
+
+  def receive = LoggingReceive {
+    case m: Message =>
+      users foreach { user =>
+        user ! m
+      }
+    case Subscribe =>
+      users += sender
+      context watch sender
+    case Terminated(user) =>
+      users -= user
+  }
+}
+
+object BoardActor {
+  lazy val board: ActorRef = Akka.system().actorOf(Props[BoardActor])
+  def apply(): ActorRef = board
+}
+
+case class Message(nickname: String, userId: Int, msg: String)
+object Subscribe
---
+++ server/app/actors/UserActor.scala
@@ -0,0 +1,44 @@
+package actors
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.event.LoggingReceive
+import play.api.libs.json.{JsValue, Json}
+
+import scala.xml.Utility
+
+class UserActor(nick: String, userId: Int, board: ActorRef, out: ActorRef) extends Actor with ActorLogging {
+
+  override def preStart() = {
+    board ! Subscribe
+    import UserActor._
+    val js = Json.obj("type" -> "info", "img" -> userId % AvatarCount)
+    out ! userId
+  }
+
+  def receive = LoggingReceive {
+    case Message(nickname, id, s) if sender == board =>
+      import UserActor._
+      if (nickname != nick) {
+        val js = Json.obj("type" -> "message", "nickname" -> nickname, "msg" -> s, "img" -> id % AvatarCount)
+        out ! js
+      }
+    case js: JsValue =>
+      (js \ "msg").validate[String] map {
+        Utility.escape
+      } foreach {
+        board ! Message(nick, userId, _)
+      }
+    case other =>
+      log.error("unhandled: " + other)
+  }
+}
+
+object UserActor {
+  val AvatarCount = 11
+  var userCount = 0
+
+  def props(nick: String)(out: ActorRef) = {
+    userCount += 1
+    Props(new UserActor(nick, userCount - 1, BoardActor(), out))
+  }
+}
