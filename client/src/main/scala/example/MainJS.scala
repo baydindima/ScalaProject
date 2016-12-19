87b6c314b992b552acb47915bb570a2bae84c65a
ilnurshug <ilnur.shugaepov@mail.ru>
2016-12-19 20:20:33 +0300

    Improvement of message handling

--- client/src/main/scala/example/MainJS.scala
+++ client/src/main/scala/example/MainJS.scala
@@ -19,15 +19,25 @@ object MainJS extends js.JSApp {
         val nick = $("#start-chat-text").`val`().asInstanceOf[String]
         val dataWsUrl = $("body").data("ws-url").asInstanceOf[String]
         val socket = new dom.WebSocket(dataWsUrl + s"?$Nick=$nick")
-
+        var myAvatarId = "0"
         socket.onmessage = {
           message: MessageEvent =>
-            println(s"message.`type` = ${message.`type`}")
-            println(s"message.data = ${message.data}")
-
+            val jsMessage = js.JSON.parse(message.data.toString)
+            message.`type` match {
+              case "message" =>
+                $("#chatAndMessage")
+                  .append("<div class=\"messageInChat\"><div class=\"messageClient\">"
+                    + "<img align='left' src=\""
+                    + "http://" + dom.window.location.hostname + ":" + dom.window.location.port
+                    + "/assets/images/" + jsMessage.img + ".png"
+                    + "\" height=\"20\" width=\"20\" />" + jsMessage.nickname + ":" + jsMessage.msg + "</div></div>")
+              case "info" =>
+                myAvatarId = jsMessage.img.toString
+              case _ =>
+                println(s"message.`type` = ${message.`type`}")
+                println(s"message.data = ${message.data}")
+            }
         }
     }
-
-
   }
 }
